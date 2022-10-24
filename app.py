cec77848f526380bad4736a89a864bfb742f589c
onfroy x <qwqlol.7696@gmail.com>
2022-10-24 15:19:50 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+ListOfMessages = []
+
+@app.route('/')
+def hello_world():
+    return 'Messenger Flask server is running! ' \
+           '<br> <a href="/status">Check status</a>'
+
+@app.route('/status')
+def status():
+    return {
+        'messages_count': len(ListOfMessages)
+    }
+# отправка сообщений
+@app.route("/api/Messanger", methods=['POST'])
+def SendMessage():
+    msg = request.json
+    print(msg)
+    # messages.append({ "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"})
+    ListOfMessages.append(msg)
+    # print(msg)
+    msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
+    return f"Сообщение отослано успешно. Всего сообщений: {len(ListOfMessages)} ", 200
+
+# получение сообщений
+@app.route("/api/Messanger/<int:id>")
+def GetMessage(id):
+    print(id)
+    if id >= 0 and id < len(ListOfMessages):
+        print(ListOfMessages[id])
+        return ListOfMessages[id], 200
+    else:
+        return "Not found", 400
+
+if __name__ == '__main__':
+    app.run()
